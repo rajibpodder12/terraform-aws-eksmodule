66532e3675631f3dac424dff45b704de6be5c583
Rajib Podder <rajibpodder12@gmail.com>
2025-01-15 07:47:58 +1100

    added description field

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,36 @@
 output "aws_vpc_id" {
+  description = "dispaly default vpc id"
   value = data.aws_vpc.vpc_id.id
 }
 
 output "aws_subnets_id" {
+  description = "display subnets associated with default vpc"
   value = data.aws_subnets.subnet_ids.ids
 }
 
 output "eks_cluster_role_arn" {
+  description = "display create eks cluster arn"
   value = aws_iam_role.eks_cluster_role.arn
 }
 
 output "eks_cluster_worker_node_role_arn" {
+  description = "display eks worker node role arn"
   value = aws_iam_role.eks_node_role.arn
 }
 
 output "alb_controller_policy_arn" {
+  description = "display alb controller ploicy arn"
   value = aws_iam_policy.alb_controller_policy.arn
 }
 
 output "aws_eks_cluster_oidc_info" {
+  description = "display oidc provider info"
   #value = split("://",data.aws_eks_cluster.eks_cluster_info.identity[0]["oidc"][*].issuer)[1]
   value = [for i in data.aws_eks_cluster.eks_cluster_info.identity[0]["oidc"][*].issuer : split("://", i)[1]][0]
 }
 
 output "ekscluster_endpoint" {
+  description = "display eks cluster endpoint"
   value = aws_eks_cluster.ekscluster.endpoint
 }
 
@@ -34,9 +41,11 @@ output "ekscluster_endpoint" {
 # }
 
 output "eks_node_group_name" {
+  description = "display eks managed node group name"
   value = aws_eks_node_group.eks_node_group.node_group_name
 }
 
 output "managed_node_group_asg" {
+  description = "display autoscaling group associated with created managed node group"
   value = [for i in aws_eks_node_group.eks_node_group.resources[0].autoscaling_groups : i.name]
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
-variable "eks_cluster_version" {}
-variable "instance_types" {}
-variable "capacity_type" {}
-variable "region" {}
+variable "eks_cluster_version" {
+    description = "input intended cluster version"
+}
+variable "instance_types" {
+    description = "input intended intended instance types"
+}
+variable "capacity_type" {
+    description = "input capacity type whether it is ON_DEMAND or SPOT"
+}
+variable "region" {
+    description = "input the AWS REGION"
+}
